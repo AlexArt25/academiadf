cd05e15323334e08e03bb2a1da1ffeec463fc4c6
AlexArt25 <alex.art.155@gmail.com>
2021-10-11 12:41:33 +0100

    Añadiendo las rutas

--- academiadf/src/App.js
+++ academiadf/src/App.js
@@ -3,7 +3,23 @@ import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 function App() {
   return (
     <Router>
-      
+      <Switch>
+        <Route path="/inicio">
+
+        </Route>
+        <Route path="/formacion">
+
+        </Route>
+        <Route path="/sobrenosotros">
+
+        </Route>
+        <Route path="/contacto">
+
+        </Route>
+        <Route path="/acceder">
+          
+        </Route>
+      </Switch> 
     </Router>
   );
 }
