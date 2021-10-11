71b5c38b53a6af68ea4857dce1ae8f2dd6651087
AlexArt25 <alex.art.155@gmail.com>
2021-10-11 13:23:55 +0100

    Importadas las paginas de la web

--- academiadf/src/App.js
+++ academiadf/src/App.js
@@ -1,24 +1,31 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
+import Inicio from './pages/Inicio';
+import Formacion from './pages/Formacion';
+import SobreNosotros from './pages/SobreNosotros';
+import Contacto from './pages/Contacto';
+import Acceder from './pages/Acceder';
 function App() {
   return (
     <Router>
+      {/* El switch muestra la primera coincidencia con la url que se le especifica */}
       <Switch>
         <Route path="/inicio">
-
+          <Inicio/>
         </Route>
         <Route path="/formacion">
-
+          <Formacion/>
         </Route>
         <Route path="/sobrenosotros">
-
+          <SobreNosotros/>
         </Route>
         <Route path="/contacto">
-
+          <Contacto/>
         </Route>
         <Route path="/acceder">
-          
+          <Acceder/>
         </Route>
+        <Redirect to="/inicio"/>
       </Switch> 
     </Router>
   );
