23b809a3c1ade315dbc2d9269c0a96f8a010bfbc
AlexArt25 <alex.art.155@gmail.com>
2021-10-14 12:28:21 +0100

    Actualizado el proyecto

--- academiadf/src/pages/InformacionCurso.jsx
+++ academiadf/src/pages/InformacionCurso.jsx
@@ -7,12 +7,36 @@ const InformacionCurso = (props) => {
     const location = useLocation();
     // Obtenemos los datos del curso
     const cursoInteres =location.state.curso.cursoInteres;
-    console.log(cursoInteres);
+    console.log(typeof cursoInteres);
     return (
         <div className="divInformacionCurso">
          <MenuHeader/>
             <main className="main">
-                
+               <div className="tablaInformacionCurso">
+                   <h1>{cursoInteres.nombre}</h1>
+                    <div className="tablaInfo">
+                        <div className="headerTabla">
+                            <p>Área</p>
+                            <p>Centro</p>
+                            <p>Nombre del Curso</p>
+                            <p>Fecha de Inicio</p>
+                            <p>Fecha de Fin</p>
+                            <p>Tipo de Curso</p>
+                            <p>Horas</p>
+                            <p>Presentacion</p>
+                        </div>
+                        <div className="cuerpoTabla">
+                        <p>{cursoInteres.familiaProfesional}</p>
+                        <p>{cursoInteres.direccion}</p>
+                        <p>{cursoInteres.nombre}</p>
+                        <p>{cursoInteres.fInicio}</p>
+                        <p>{cursoInteres.fFinal}</p>
+                        <p>{cursoInteres.destinatarios}</p>
+                        <p>{cursoInteres.horasLectivas}</p>
+                        <p>{cursoInteres.descripcion}</p>
+                        </div>
+                    </div>
+               </div>
             </main>   
         </div>
     )
