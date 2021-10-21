17dd7f490fda9e472b041325620146b2ab411d67
AlexArt25 <alex.art.155@gmail.com>
2021-10-21 18:09:23 +0100

    Añadida la funcionalidad del formulario y corregido el css

--- academiadf/src/pages/MasInformacion.jsx
+++ academiadf/src/pages/MasInformacion.jsx
@@ -15,6 +15,13 @@ const [telefono,setTelefono] = useState("");
 const [email,setEmail] = useState("");
 const [comentario,setComentario] = useState("");
 const [condicionesLegales,setCondicionesLegales] = useState(false);
+const [error,setError] = useState({
+    errorNombre:"",
+   errorTelefono:"",
+  errorEmail:"",
+  errorComentario:"",
+ errorCondicionesLegales:""
+});
 
 // Gestores de los campos del formulario
 
@@ -44,15 +51,36 @@ const gestorCondicionesLegales = (e) =>{
 const submit = (e)=>{
     e.preventDefault();
 
-    console.log(nombre+""+telefono+""+email+""+comentario+""+condicionesLegales);
-
+    if(nombre.length <=0 || !isNaN(nombre)){
+        error.errorNombre="*";
+    }
+    if(isNaN(telefono) || telefono.length>9 || telefono.length<9 || telefono === ""){
+        error.errorTelefono="*";
+    }
+    if(email===""){
+        error.errorEmail="";
+    }
+    if(comentario===""){
+        error.errorComentario="*";
+    }
+    if(condicionesLegales === false){
+        error.errorCondicionesLegales="*";
+    }
+    if(error.errorNombre==="" && error.errorTelefono==="" && error.errorEmail==="" && error.errorComentario==="" && error.errorCondicionesLegales===""){
+        console.log("Mensaje Enviado");
+    }
     // Resetear los valores de los campos del formulario
     setNombre("");
     setTelefono("");
     setEmail("");
     setComentario("");
     setCondicionesLegales(false);
-
+    error.errorNombre="";
+    error.errorTelefono="";
+    error.errorEmail="";
+    error.errorMensaje="";
+    error.errorCondicionesLegales="";
+    console.log(error.errorNombre);
 }
 
 return (
@@ -71,33 +99,39 @@ return (
                     <div className="form-group col-12 col-md-4">
                         <label htmlFor="nombre">Nombre:</label>
                         <input type="text" name="nombre" id="nombre" value={nombre} onChange={gestorNombre}/>
+                        <span className="error">{error.errorNombre}</span>
                     </div>
                     <div className="form-group col-12 col-md-4">
                         <label htmlFor="email">Correo:</label>
                         <input type="email" name="email" id="email" value={email} onChange={gestorEmail}/>
+                        <span className="error">{error.errorEmail}</span>
                     </div>
                     <div className="form-group col-12 col-md-4">
                         <label htmlFor="telefono">Telefono:</label>
                         <input type="tel" name="telefono" id="telefono" maxLength="9" value={telefono} onChange={gestorTelefono}/>
+                        <span className="error">{error.errorTelefono}</span>
                     </div>
                     <div className="form-group comentarioMasInformacion col-12">
                         <label htmlFor="comentario">Comentario:</label>
                         <textarea name="comentario" id="comentario" value={comentario} onChange={gestorComentario}>
 
                         </textarea>
+                        <span className="error">{error.errorMensaje}</span>
                     </div>
-                    <div className="headerPreinscripcion col-12">
+                    <div className="headerMasInformacion col-12">
                             <h3>Condiciones Legales</h3>
                         </div>
-                        <div className="datosPreinscripcion">
-                            <div className="form-group col-12">
-                            <input type="checkbox" className="col-1" name="condicionesLegales" id="condicionesLegales" onChange={gestorCondicionesLegales}/>
-                            <p className="col-10">He leido y acepto las <a href="https://www.academiadf.com/adfv2012/ADF_Politica.aspx">Condiciones Legales</a></p>
+                        <div className="datosMasInformacion condicionesLegales">
+                            <div className="form-group">
+                            <input type="checkbox" name="condicionesLegales" id="condicionesLegales" onChange={gestorCondicionesLegales}/>
+                            <p>He leido y acepto las <a href="https://www.academiadf.com/adfv2012/ADF_Politica.aspx">Condiciones Legales</a></p>
+                            <span className="error">{error.errorCondicionesLegales}</span>
                             </div>
                         </div>
                 </div>
                 <button>Enviar</button>
             </form>
+            {error.errorNombre};
         </div>
     </main>
 </div>
