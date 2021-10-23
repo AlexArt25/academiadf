8ade7741649ef3646ed5b2b33e7f1605b57e64fe
AlexArt25 <alex.art.155@gmail.com>
2021-10-23 10:19:09 +0100

    CSS MasInformacion y Formulario

--- academiadf/src/pages/MasInformacion.jsx
+++ academiadf/src/pages/MasInformacion.jsx
@@ -15,13 +15,11 @@ const [telefono,setTelefono] = useState("");
 const [email,setEmail] = useState("");
 const [comentario,setComentario] = useState("");
 const [condicionesLegales,setCondicionesLegales] = useState(false);
-const [error,setError] = useState({
-    errorNombre:"",
-   errorTelefono:"",
-  errorEmail:"",
-  errorComentario:"",
- errorCondicionesLegales:""
-});
+const [errorNombre,setErrorNombre] = useState("");
+const [errorEmail,setErrorEmail] = useState("");
+const [errorTelefono,setErrorTelefono] = useState("");
+const [errorComentario,setErrorComentario] = useState("");
+const [errorCondicionesLegales,setErrorCondicionesLegales] = useState("");
 
 // Gestores de los campos del formulario
 
@@ -52,22 +50,34 @@ const submit = (e)=>{
     e.preventDefault();
 
     if(nombre.length <=0 || !isNaN(nombre)){
-        error.errorNombre="*";
+        setErrorNombre("*");
+    }else{
+        setErrorNombre("");
     }
     if(isNaN(telefono) || telefono.length>9 || telefono.length<9 || telefono === ""){
-        error.errorTelefono="*";
+        setErrorTelefono("*");
+    }else{
+        setErrorTelefono("");
     }
     if(email===""){
-        error.errorEmail="";
+        setErrorEmail("*");
+    }else{
+        setErrorEmail("");
     }
     if(comentario===""){
-        error.errorComentario="*";
+        setErrorComentario("*");
+    }else{
+        setErrorComentario("");
     }
     if(condicionesLegales === false){
-        error.errorCondicionesLegales="*";
+        setErrorCondicionesLegales("*");
+    }else{
+        setErrorCondicionesLegales("");
     }
-    if(error.errorNombre==="" && error.errorTelefono==="" && error.errorEmail==="" && error.errorComentario==="" && error.errorCondicionesLegales===""){
+    if(errorNombre==="" && errorTelefono==="" && errorEmail==="" && errorComentario==="" && errorCondicionesLegales===""){
         console.log("Mensaje Enviado");
+    }else{
+        console.log("Algo ha fallado");
     }
     // Resetear los valores de los campos del formulario
     setNombre("");
@@ -75,12 +85,6 @@ const submit = (e)=>{
     setEmail("");
     setComentario("");
     setCondicionesLegales(false);
-    error.errorNombre="";
-    error.errorTelefono="";
-    error.errorEmail="";
-    error.errorMensaje="";
-    error.errorCondicionesLegales="";
-    console.log(error.errorNombre);
 }
 
 return (
@@ -99,24 +103,24 @@ return (
                     <div className="form-group col-12 col-md-4">
                         <label htmlFor="nombre">Nombre:</label>
                         <input type="text" name="nombre" id="nombre" value={nombre} onChange={gestorNombre}/>
-                        <span className="error">{error.errorNombre}</span>
+                        <span className="error">{errorNombre}</span>
                     </div>
                     <div className="form-group col-12 col-md-4">
                         <label htmlFor="email">Correo:</label>
                         <input type="email" name="email" id="email" value={email} onChange={gestorEmail}/>
-                        <span className="error">{error.errorEmail}</span>
+                        <span className="error">{errorEmail}</span>
                     </div>
                     <div className="form-group col-12 col-md-4">
                         <label htmlFor="telefono">Telefono:</label>
                         <input type="tel" name="telefono" id="telefono" maxLength="9" value={telefono} onChange={gestorTelefono}/>
-                        <span className="error">{error.errorTelefono}</span>
+                        <span className="error">{errorTelefono}</span>
                     </div>
                     <div className="form-group comentarioMasInformacion col-12">
-                        <label htmlFor="comentario">Comentario:</label>
+                        <label htmlFor="comentario col-12">Comentario:</label>
                         <textarea name="comentario" id="comentario" value={comentario} onChange={gestorComentario}>
 
                         </textarea>
-                        <span className="error">{error.errorMensaje}</span>
+                        <span className="error">{errorComentario}</span>
                     </div>
                     <div className="headerMasInformacion col-12">
                             <h3>Condiciones Legales</h3>
@@ -125,13 +129,12 @@ return (
                             <div className="form-group">
                             <input type="checkbox" name="condicionesLegales" id="condicionesLegales" onChange={gestorCondicionesLegales}/>
                             <p>He leido y acepto las <a href="https://www.academiadf.com/adfv2012/ADF_Politica.aspx">Condiciones Legales</a></p>
-                            <span className="error">{error.errorCondicionesLegales}</span>
+                            <span className="error">{errorCondicionesLegales}</span>
                             </div>
                         </div>
                 </div>
                 <button>Enviar</button>
             </form>
-            {error.errorNombre};
         </div>
     </main>
 </div>
