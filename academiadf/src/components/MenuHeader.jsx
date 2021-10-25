91a4e4054945780a31fd7418d1cd96c3e35ced45
AlexArt25 <alex.art.155@gmail.com>
2021-10-25 09:26:43 +0100

    Header Terminado

--- academiadf/src/components/MenuHeader.jsx
+++ academiadf/src/components/MenuHeader.jsx
@@ -6,24 +6,24 @@ const MenuHeader = () => {
     return (
         <div className="divHeader">
           <Navbar className="divNavbar" expand="md">
-            <Container className="navbar">
-               
-                <a className="divLogo" href="/inicio">
-                    <img src={logo} alt="Logo" />
-                </a>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link className="link" href="/inicio">Inicio</Nav.Link>
-                    <Nav.Link className="link" href="/formacion">Formacion</Nav.Link>
-                    <Nav.Link className="link" href="/empleo">Empleo</Nav.Link>
-                    <NavDropdown title="Sobre Nosotros" className="link" id="basic-nav-dropdown">
-                    {/* <NavDropdown.Item className="dropDownMenu" href="/Contacto">Contacto</NavDropdown.Item> */}
-                    </NavDropdown>
-                    <Nav.Link className="link" href="/acceder">Acceder</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
+                <Container className="navbar">  
+                    <a className="divLogo" href="/inicio">
+                        <img src={logo} alt="Logo" />
+                    </a>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link className="link" href="/inicio">Inicio</Nav.Link>
+                            <Nav.Link className="link" href="/formacion">Formacion</Nav.Link>
+                            <Nav.Link className="link" href="/empleo">Empleo</Nav.Link>
+                            <NavDropdown title="Sobre Nosotros" className="link" id="basic-nav-dropdown">
+                                <NavDropdown.Item className="dropDownMenu" href="/sobrenosotros">Sobre Nosotros</NavDropdown.Item>
+                                <NavDropdown.Item className="dropDownMenu" href="/Contacto">Contacto</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link className="link" href="/acceder">Acceder</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </div>
     )
